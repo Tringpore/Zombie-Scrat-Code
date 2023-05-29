ab55b5cabd25baaee932cbe64dc3d817e59d1145
Jj Kwan <tringpore@gmail.com>
2023-05-29 14:51:34 +0800

    port allocation template

---
+++ include/globals.hpp
@@ -0,0 +1,16 @@
+#ifndef _GLOBALS_HPP_
+#define _GLOBALS_HPP_
+
+#define FLport
+#define BLport
+#define FRport
+#define BRport
+
+#define cataLport
+#define cataRport
+
+#define intakeport
+
+//#define matchloaderport
+
+#endif 
